b3cb0d6d43d0f5ddad675a1fb1c3b83d2230ff66
Filip Chalupa <chalupa.filip@gmail.com>
2024-12-14 17:01:17 +0100

    Change starter

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,7 @@
-import { reactRouter } from '@react-router/dev/vite'
-import autoprefixer from 'autoprefixer'
-import tailwindcss from 'tailwindcss'
+import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
 
+// https://vite.dev/config/
 export default defineConfig({
-	css: {
-		postcss: {
-			plugins: [tailwindcss, autoprefixer],
-		},
-	},
-	plugins: [reactRouter(), tsconfigPaths()],
+	plugins: [react()],
 })
