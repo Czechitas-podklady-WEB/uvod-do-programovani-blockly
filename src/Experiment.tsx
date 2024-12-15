52eb25a961ac830a09a64714cab583d58c5b43ca
Filip Chalupa <chalupa.filip@gmail.com>
2024-12-15 16:50:51 +0100

    Add Material UI

--- src/Experiment.tsx
+++ src/Experiment.tsx
@@ -1,5 +1,10 @@
+import Button from '@mui/material/Button'
 import type { FunctionComponent } from 'react'
 
 export const Experiment: FunctionComponent = () => {
-	return <div>Experiment</div>
+	return (
+		<div>
+			Experiment <Button variant="contained">Hello world</Button>;
+		</div>
+	)
 }
