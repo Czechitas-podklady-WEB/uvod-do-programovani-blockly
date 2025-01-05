ed142ed3e37b9f7b3e34aa16e97a6da7389c001a
Filip Chalupa <chalupa.filip@gmail.com>
2025-01-05 14:40:52 +0100

    Add Evolu dependency

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	optimizeDeps: {
+		exclude: ['@evolu/react', '@sqlite.org/sqlite-wasm'],
+		include: ['react-dom'],
+	},
+	worker: { format: 'es' },
 })
