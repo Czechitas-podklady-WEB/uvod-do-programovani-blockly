f1fa13e2876b63358ee0eb495d2771566b088a0e
Filip Chalupa <chalupa.filip@gmail.com>
2025-01-07 23:40:01 +0100

    Move padding

--- src/utilities/getLevelIdentifier.ts
+++ src/utilities/getLevelIdentifier.ts
@@ -5,13 +5,15 @@ export const LevelIdentifier = id('LevelIdentifier')
 export type LevelIdentifier = typeof LevelIdentifier.Type
 
 // Based on https://stackoverflow.com/a/15710692
-const hashCode = (input: string) =>
-	Math.abs(
+const hashCode = (input: string) => {
+	const base = Math.abs(
 		input.split('').reduce((a, b) => {
 			a = (a << 5) - a + b.charCodeAt(0)
 			return a & a
 		}, 0),
 	).toString(16)
+	return `${''.padStart(21 - base.length, '0')}${base}`
+}
 
 const levelIdentifiers: {
 	[groupKey: GroupKey]: {
@@ -22,10 +24,7 @@ const levelIdentifiers: {
 export const getLevelIdentifier = (groupKey: GroupKey, levelKey: LevelKey) => {
 	const group = (levelIdentifiers[groupKey] ??= {})
 	const levelIdentifier = (group[levelKey] ??= LevelIdentifier.make(
-		(() => {
-			const base = hashCode(`${groupKey}_${levelKey}`)
-			return `${''.padStart(21 - base.length, '0')}${base}`
-		})(),
+		(() => hashCode(`${groupKey}_${levelKey}`))(),
 	))
 	return levelIdentifier
 }
