47384445d5d6b1a2f4acaafdb09a7ea661930cab
Filip Chalupa <chalupa.filip@gmail.com>
2025-01-07 23:39:06 +0100

    Add hashing

--- src/utilities/getLevelIdentifier.ts
+++ src/utilities/getLevelIdentifier.ts
@@ -4,6 +4,15 @@ import { GroupKey, LevelKey } from '../data/levels'
 export const LevelIdentifier = id('LevelIdentifier')
 export type LevelIdentifier = typeof LevelIdentifier.Type
 
+// Based on https://stackoverflow.com/a/15710692
+const hashCode = (input: string) =>
+	Math.abs(
+		input.split('').reduce((a, b) => {
+			a = (a << 5) - a + b.charCodeAt(0)
+			return a & a
+		}, 0),
+	).toString(16)
+
 const levelIdentifiers: {
 	[groupKey: GroupKey]: {
 		[levelKey: LevelKey]: LevelIdentifier
@@ -14,8 +23,8 @@ export const getLevelIdentifier = (groupKey: GroupKey, levelKey: LevelKey) => {
 	const group = (levelIdentifiers[groupKey] ??= {})
 	const levelIdentifier = (group[levelKey] ??= LevelIdentifier.make(
 		(() => {
-			const base = `${groupKey}_${levelKey}`
-			return `${base}${''.padStart(21 - base.length, '0')}`
+			const base = hashCode(`${groupKey}_${levelKey}`)
+			return `${''.padStart(21 - base.length, '0')}${base}`
 		})(),
 	))
 	return levelIdentifier
