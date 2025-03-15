0fe3af6021ceb7881d1a0c34e961033ca06461f9
Filip Chalupa <chalupa.filip@gmail.com>
2025-03-15 19:47:03 +0100

    Thumbnails: improve generator

--- src/generate-thumbnails.ts
+++ src/generate-thumbnails.ts
@@ -1,4 +1,4 @@
-import { mkdir, rm } from 'node:fs/promises'
+import { mkdir, rm, writeFile } from 'node:fs/promises'
 import { resolve } from 'node:path'
 import puppeteer from 'puppeteer'
 import { delay } from './utilities/delay.ts'
@@ -21,16 +21,35 @@ await rm(basePath, {
 })
 await mkdir(basePath)
 
+let exportFileContent = ''
+const keyToUrl = new Map<string, string>()
+
 const levels = await page.$$('div[data-level-key]')
 console.log('Levels found:', levels.length)
+let lastNameBase = 0
 for (const level of levels) {
 	const levelKey = await level.evaluate((element) =>
 		element.getAttribute('data-level-key'),
 	)
+	if (levelKey === null) {
+		throw new Error('Level key not found')
+	}
 	console.log('Capturing level:', levelKey)
 	await level.screenshot({
 		path: resolve(basePath, `${levelKey}.png`),
 	})
+	const name = `l_${lastNameBase++}`
+	exportFileContent += `import ${name} from './${levelKey}.png'\n`
+	keyToUrl.set(levelKey, name)
 }
+
+exportFileContent += '\nexport const thumbnails = new Map([\n'
+for (const [key, url] of keyToUrl) {
+	exportFileContent += `\t['${key}', ${url}],\n`
+}
+exportFileContent += '])\n'
+
+console.log('Generating thumbnails.ts')
+await writeFile(resolve(basePath, 'thumbnails.ts'), exportFileContent)
 
 await browser.close()
