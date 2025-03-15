10925d67f7471ecc844d5348714765cf0a55d5ef
Filip Chalupa <chalupa.filip@gmail.com>
2025-03-15 19:47:15 +0100

    Generate thumbnails

---
+++ src/assets/thumbnails/thumbnails.ts
@@ -0,0 +1,43 @@
+import l_0 from './0.png'
+import l_1 from './1.png'
+import l_2 from './2.png'
+import l_3 from './3.png'
+import l_4 from './4.png'
+import l_5 from './5.png'
+import l_6 from './6.png'
+import l_7 from './7.png'
+import l_8 from './8.png'
+import l_9 from './9.png'
+import l_10 from './10.png'
+import l_11 from './11.png'
+import l_12 from './12.png'
+import l_13 from './13.png'
+import l_14 from './14.png'
+import l_15 from './15.png'
+import l_16 from './16.png'
+import l_17 from './17.png'
+import l_18 from './18.png'
+import l_19 from './19.png'
+
+export const thumbnails = new Map([
+	['0', l_0],
+	['1', l_1],
+	['2', l_2],
+	['3', l_3],
+	['4', l_4],
+	['5', l_5],
+	['6', l_6],
+	['7', l_7],
+	['8', l_8],
+	['9', l_9],
+	['10', l_10],
+	['11', l_11],
+	['12', l_12],
+	['13', l_13],
+	['14', l_14],
+	['15', l_15],
+	['16', l_16],
+	['17', l_17],
+	['18', l_18],
+	['19', l_19],
+])
