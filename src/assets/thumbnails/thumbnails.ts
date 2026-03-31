6b32005b326c534c9379a19a84c9ca57f9f90c8f
Filip Chalupa <chalupa.filip@gmail.com>
2026-04-01 00:36:11 +0200

    Update thumbnails

--- src/assets/thumbnails/thumbnails.ts
+++ src/assets/thumbnails/thumbnails.ts
@@ -18,6 +18,37 @@ import l_16 from './16.png'
 import l_17 from './17.png'
 import l_18 from './18.png'
 import l_19 from './19.png'
+import l_20 from './20.png'
+import l_21 from './21.png'
+import l_22 from './22.png'
+import l_23 from './23.png'
+import l_24 from './24.png'
+import l_25 from './25.png'
+import l_26 from './26.png'
+import l_27 from './27.png'
+import l_28 from './28.png'
+import l_29 from './29.png'
+import l_30 from './30.png'
+import l_31 from './31.png'
+import l_32 from './32.png'
+import l_33 from './33.png'
+import l_34 from './34.png'
+import l_35 from './35.png'
+import l_36 from './36.png'
+import l_37 from './37.png'
+import l_38 from './38.png'
+import l_39 from './39.png'
+import l_40 from './40.png'
+import l_41 from './41.png'
+import l_42 from './42.png'
+import l_43 from './43.png'
+import l_44 from './44.png'
+import l_45 from './45.png'
+import l_46 from './46.png'
+import l_47 from './47.png'
+import l_48 from './48.png'
+import l_49 from './49.png'
+import l_50 from './50.png'
 
 export const thumbnails = new Map([
 	['0', l_0],
@@ -40,4 +71,35 @@ export const thumbnails = new Map([
 	['17', l_17],
 	['18', l_18],
 	['19', l_19],
+	['20', l_20],
+	['21', l_21],
+	['22', l_22],
+	['23', l_23],
+	['24', l_24],
+	['25', l_25],
+	['26', l_26],
+	['27', l_27],
+	['28', l_28],
+	['29', l_29],
+	['30', l_30],
+	['31', l_31],
+	['32', l_32],
+	['33', l_33],
+	['34', l_34],
+	['35', l_35],
+	['36', l_36],
+	['37', l_37],
+	['38', l_38],
+	['39', l_39],
+	['40', l_40],
+	['41', l_41],
+	['42', l_42],
+	['43', l_43],
+	['44', l_44],
+	['45', l_45],
+	['46', l_46],
+	['47', l_47],
+	['48', l_48],
+	['49', l_49],
+	['50', l_50],
 ])
